45e940577d2c4cdaadf17dc7f79c12d510145307
theozebua <ronizebua.rz@gmail.com>
2022-05-26 16:27:48 +0700

    Refactor script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,56 @@
-const sideNav = document.querySelectorAll(".sidenav");
+const sideNav = document.querySelectorAll('.sidenav')
 M.Sidenav.init(sideNav, {
   inDuration: 650,
   outDuration: 500,
-});
+})
 
-const slider = document.querySelectorAll(".slider");
+const slider = document.querySelectorAll('.slider')
 M.Slider.init(slider, {
   indicators: true,
   transition: 600,
   interval: 3000,
-});
+})
 
-const parallax = document.querySelectorAll(".parallax");
-M.Parallax.init(parallax);
+const parallax = document.querySelectorAll('.parallax')
+M.Parallax.init(parallax)
 
-const materialbox = document.querySelectorAll(".materialboxed");
-M.Materialbox.init(materialbox);
+const materialbox = document.querySelectorAll('.materialboxed')
+M.Materialbox.init(materialbox)
 
-const scrollspy = document.querySelectorAll(".scrollspy");
+const scrollspy = document.querySelectorAll('.scrollspy')
 M.ScrollSpy.init(scrollspy, {
   scrollOffset: 65,
-});
+})
 
-const fixedActionBtn = document.querySelectorAll(".fixed-action-btn");
+const fixedActionBtn = document.querySelectorAll('.fixed-action-btn')
 M.FloatingActionButton.init(fixedActionBtn, {
   toolbarEnabled: true,
-});
+})
 
 const scrollEffect = () => {
-  const items = document.querySelectorAll(".scale-transition");
-  let screenPosition = window.innerHeight / 1.1;
+  const items = document.querySelectorAll('.scale-transition')
+  let screenPosition = window.innerHeight / 1.1
   for (i = 0; i < items.length; i++) {
-    let itemsPosition = items[i].getBoundingClientRect().top;
+    let itemsPosition = items[i].getBoundingClientRect().top
     if (itemsPosition < screenPosition) {
-      items[i].classList.remove("scale-out");
+      items[i].classList.remove('scale-out')
     } else {
-      items[i].classList.add("scale-out");
+      items[i].classList.add('scale-out')
     }
   }
-};
+}
 
 const scrollEffectBackToTop = () => {
-  const backToTopBtn = document.querySelector(".custom-backtotop-btn");
-  let homeSLiderPosition = document.getElementById("home").getBoundingClientRect().top;
-  let screenPosition = window.innerHeight / 1.1;
+  const backToTopBtn = document.querySelector('.custom-backtotop-btn')
+  let homeSLiderPosition = document.getElementById('home').getBoundingClientRect().top
+  let screenPosition = window.innerHeight / 1.1
   if (homeSLiderPosition < screenPosition) {
-    backToTopBtn.classList.add("scale-in");
-    backToTopBtn.classList.remove("scale-out");
+    backToTopBtn.classList.add('scale-in')
+    backToTopBtn.classList.remove('scale-out')
   } else {
-    backToTopBtn.classList.add("scale-out");
-    backToTopBtn.classList.remove("scale-in");
+    backToTopBtn.classList.add('scale-out')
+    backToTopBtn.classList.remove('scale-in')
   }
-};
+}
 
-window.addEventListener("scroll", scrollEffect);
-window.addEventListener("scroll", scrollEffectBackToTop);
+window.addEventListener('scroll', [scrollEffect, scrollEffectBackToTop])
